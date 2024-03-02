6b0686a98f19b5e1bb308a57dcc22d72e30b912e
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-03-02 15:41:02 +0400

    make contact us responsive

--- src/pages/ContactUs/styles.ts
+++ src/pages/ContactUs/styles.ts
@@ -2,6 +2,9 @@ import styled from 'styled-components'
 
 export const ContactUsContainer = styled.div`
   margin: 125px 0px 85px 315px;
+  @media only screen and (max-width: 1180px) {
+    margin: 100px auto;
+  }
 `
 
 export const ContactUsInfo = styled.div`
@@ -37,6 +40,30 @@ export const ContactUsInfo = styled.div`
       line-height: 28px;
     }
   }
+  @media only screen and (max-width: 1180px) {
+    margin: 0 auto;
+  }
+  @media only screen and (max-width: 660px) {
+    width: 330px;
+    h1 {
+      text-align: center;
+      font-size: 25px;
+    }
+    p {
+      text-align: center;
+      font-size: 18px;
+    }
+    > div {
+      justify-content: space-around;
+      h4 {
+        font-size: 20px;
+        margin-bottom: 13px;
+      }
+      p {
+        font-size: 12px;
+      }
+    }
+  }
 `
 
 export const ContactUsInputs = styled.div`
@@ -69,6 +96,16 @@ export const ContactUsInputs = styled.div`
     background-color: #000;
     border-radius: 10px;
   }
+  @media only screen and (max-width: 1180px) {
+    margin: 0 auto;
+  }
+  @media only screen and (max-width: 660px) {
+    width: 330px;
+  }
+  @media only screen and (max-width: 460px) {
+    padding: 40px;
+    width: 230px;
+  }
 `
 
 export const ContactInput = styled.input`
@@ -89,6 +126,7 @@ export const ContactMapContainer = styled.div`
   right: 0;
   top: 195px;
   width: 945px;
+  z-index: -1;
   img {
     margin-bottom: 90px;
   }
